0b95ea7b7f25f8b91cd4317ac6fc19127531f068
Tiago Soares <tsoares.sjc@gmail.com>
2018-04-01 23:46:24 -0300

    Novas resoluções adicionadas.

---
+++ ucoder/1232.py
@@ -0,0 +1,11 @@
+#https://ucoder.com.br/problems/1232/
+
+a = input()
+if a == "M":
+    print("Bom Dia!")
+elif a == "N":
+    print("Boa Noite!")
+elif a == "V":
+    print("Boa Tarde!")
+else:
+    print("Valor Invalido!")
